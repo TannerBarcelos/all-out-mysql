66d11435e539dda954207faabf26d6e50779ef5f
Tanner Barcelos <barcelos.m.tanner@gmail.com>
2023-11-15 17:56:22 -0800

    Init commit

---
+++ intro-to-mysql.sql
@@ -0,0 +1,11 @@
+-- Shows all databases within your server
+show databases;
+
+-- Use a specific database
+use mysql;
+
+-- Show all the tables in the database
+show tables;
+
+-- Select some columns from some table in the database
+select * from default_roles;
