e0d2c699c7968848902dd490fd656c7d4704776a
Tanner Barcelos <barcelos.m.tanner@gmail.com>
2023-11-15 18:08:51 -0800

    Further work before re-learning types

--- introduction/intro-to-mysql.sql
+++ introduction/intro-to-mysql.sql
@@ -1,13 +1,13 @@
 -- Shows all databases within your server
-show databases;
+SHOW DATABASES;
 
--- Create a database
+-- Create a database - A DB is a collection of objects such as tables, functions, procedures, indexes etc.
 CREATE DATABASE pet_shop;
 CREATE DATABASE example;
 
 -- Use a specific database (not needed when using a GUI i.e TablePlus, DBVisualizer) as you select the databases you want to use to give your editor context. 
--- This is needed in the CLI, though.
+-- This is needed in the CLI, though. The command is used to tell the RDBMS where it needs to go / be to execute SQL
 USE pet_shop;
 
--- See all tables in a database
+-- See all tables in a database (at this point we have none)
 SHOW TABLES;
